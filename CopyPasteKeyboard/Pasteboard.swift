2d1b4f9d8a1604dbf220fac884137c32a90152b4
tochiba <c104kn@gmail.com>
2017-03-27 23:57:12 +0900

    Entity作成

--- CopyPasteKeyboard/Pasteboard.swift
+++ CopyPasteKeyboard/Pasteboard.swift
@@ -9,9 +9,37 @@ import UIKit
 import Foundation
 import RealmSwift
 
-final class Pasteboard: Object {
+enum GenreType: Int {
+    case none, text, url, image
+}
+
+final class PasteboardEntity: Object {
     dynamic var text = ""
     dynamic var url = ""
     dynamic var image = UIImage()
-    dynamic var type = 0
+    dynamic var type = GenreType.none.rawValue
+    
+    convenience init(pasteboard: Pasteboard) {
+        self.init()
+        text = pasteboard.text
+        url = pasteboard.url
+        image = pasteboard.image
+        type = pasteboard.type.rawValue
+    }
+    
+    func toStruct() -> Pasteboard {
+        var pasteboard = Pasteboard()
+        pasteboard.text = text
+        pasteboard.url = url
+        pasteboard.image = image
+        pasteboard.type = GenreType(rawValue: type) ?? .none
+        return pasteboard
+    }
+}
+
+struct Pasteboard {
+    var text = ""
+    var url = ""
+    var image = UIImage()
+    var type = GenreType.none
 }
